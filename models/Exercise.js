9ec23aa45a7621d514b2e3942e853740f31bc87e
adam stevenson <adamscdcm@gmail.com>
2021-06-03 21:57:47 -0700

    fixed workouts

--- models/Exercise.js
+++ models/Exercise.js
@@ -47,7 +47,7 @@ const exerciseSchema = new Schema(
 exerciseSchema.virtual("totalDuration").get(function() {
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
-  });
+  },0);
 })
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
--- models/Workout.js
+++ models/Workout.js
@@ -1,14 +1,14 @@
-// const mongoose = require("mongoose");
-// const Exercise = require('./Exercise');
+const mongoose = require("mongoose");
+const Exercise = require('./Exercise');
 
-// const workoutSchema = new mongoose.Schema({
-//   day: {
-//     type: Date,
-//     default: Date.now()
-//   },
-//   exercises: [Exercise.schema]
-// });
+const workoutSchema = new mongoose.Schema({
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+  exercises: [Exercise.schema]
+});
 
-// const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
-// module.exports = Workout;
+module.exports = Workout;
